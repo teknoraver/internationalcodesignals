d752cc5479d5e475ecc522c5e86d5c7a9394ee52
Matteo Croce <rootkit85@yahoo.it>
2012-11-24 21:50:03 +0100

    first import

---
+++ src/net/teknoraver/ics/ICS.java
@@ -0,0 +1,185 @@
+package net.teknoraver.ics;
+
+import java.util.Locale;
+
+import android.app.Activity;
+import android.graphics.Point;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
+import android.util.DisplayMetrics;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class ICS extends Activity implements Runnable, OnInitListener, OnTouchListener {
+	private final int flagsId[] = new int[] {
+			R.drawable.n0, R.drawable.n1, R.drawable.n2, R.drawable.n3, R.drawable.n4, R.drawable.n5, R.drawable.n6, R.drawable.n7,
+			R.drawable.n8, R.drawable.n9, 0, 0, 0, 0, 0, 0, 0,
+			R.drawable.a, R.drawable.b, R.drawable.c, R.drawable.d, R.drawable.e, R.drawable.f, R.drawable.g, R.drawable.h,
+			R.drawable.i, R.drawable.j, R.drawable.k, R.drawable.l, R.drawable.m, R.drawable.n, R.drawable.o, R.drawable.p,
+			R.drawable.q, R.drawable.r, R.drawable.s, R.drawable.t, R.drawable.u, R.drawable.v, R.drawable.w, R.drawable.x,
+			R.drawable.y, R.drawable.z
+	};
+	private final String codes[] = new String[] {
+			"ZERO", "ONE", "TWO", "THREE", "FOWER", "FIFE", "SIX", "SEVEN",
+			"EIGHT", "NINER", null, null, null, null, null, null, null,
+			"ALPHA", "BRAVO", "CHARLIE", "DELTA", "ECHO", "FOXTROT", "GOLF", "HOTEL",
+			"INDIA", "JULIET", "KILO", "LIMA", "MIKE", "NOVEMBER", "OSCAR", "PAPA",
+			"QUEBEC", "ROMEO", "SIERRA", "TANGO", "UNIFORM", "VICTOR", "WHISKEY", "X-RAY",
+			"YANKEE", "ZULU"
+	};
+//	private final Drawable flags[] = new Drawable[codes.length];
+
+	static final String stringcmd = "string";
+	static final String flagcmd = "showflag";
+	static final String textcmd = "showtext";
+	static final String speakcmd = "speak";
+	static final String speedcmd = "speed";
+	private TextView code;
+	private TextToSpeech tts;
+	private int pos;
+	private String string;
+	private Handler handler;
+	private int speed;
+	boolean showflag;
+	boolean showtext;
+
+	private final Runnable Updater = new Runnable() {
+		private boolean show = true;
+		@Override
+		public void run() {
+			if(show)
+				showChar(string.charAt(pos++));
+			else
+				showChar(' ');
+			show = !show;
+		}
+	};
+
+	@Override
+	public void onCreate(final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.show);
+
+		final LinearLayout ll = (LinearLayout)findViewById(R.id.showlayout);
+		code = (TextView)findViewById(R.id.code);
+
+		showflag = getIntent().getBooleanExtra(flagcmd, true);
+		showtext = getIntent().getBooleanExtra(textcmd, true);
+		if(getIntent().getBooleanExtra(speakcmd, true))
+			tts = new TextToSpeech(this, this);
+		string = getIntent().getStringExtra(stringcmd);
+
+		pos = 0;
+		speed = getIntent().getIntExtra(speedcmd, 5);
+
+		handler = new Handler();
+
+		if(speed < 0) {
+			handler.post(Updater);
+			ll.setOnTouchListener(this);
+		} else
+			new Thread(this).start();
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		if(tts != null)
+			tts.shutdown();
+	}
+
+	private Point getSize(char c) {
+		// FIXME use dynamic values from the Drawable
+		final int LW = 480, LH = 480;
+		final int NW = 800, NH = 400;
+		int iw = 1, ih = 1;
+
+		if(c >= '0' && c <= '9') {
+			iw = NW;
+			ih = NH;
+		} else if(c >= 'A' && c <= 'Z'){
+			iw = LW;
+			ih = LH;
+		}
+
+		DisplayMetrics metrics = getResources().getDisplayMetrics();
+
+		int sw = metrics.widthPixels, sh = metrics.heightPixels;
+		if(showtext)
+			sh = sh * 3 / 4;
+
+		if(iw / ih > sw / sh)
+			return new Point(sw, sw * ih / iw);
+		else
+			return new Point(sh * iw / ih, sh);
+	}
+
+	private Drawable getFlag(final char c) {
+		Drawable flags = getResources().getDrawable(flagsId[c - '0']);
+		Point p = getSize(c);
+		flags.setBounds(0, 0, p.x, p.y);
+		return flags;
+	}
+
+	private void showChar(final char c) {
+		Drawable flag = null;
+		String text = null;
+		if(c >= '0' && c <= '9' || c >= 'A' && c <= 'Z') {
+			if(showflag)
+				flag = getFlag(c);
+			if(showtext)
+				text = codes[c - '0'];
+			if(tts != null) {
+				String letter = codes[c - '0'];
+				if(c >= '1' && c <= '3' || c == '6')
+					letter = new String(new char[]{c});
+				tts.speak(letter, 0, null);
+			}
+		}
+		code.setText(text);
+		code.setCompoundDrawables(null, flag, null, null);
+	}
+
+	@Override
+	public void run() {
+		pos = 0;
+		try {
+			while(pos < string.length() && !isFinishing()) {
+				handler.post(Updater);
+				Thread.sleep(speed * 100);
+				handler.post(Updater);
+				Thread.sleep(speed * 10);
+			}
+		} catch (final InterruptedException e) {
+			e.printStackTrace();
+		}
+		string = null;
+		finish();
+	}
+
+	@Override
+	public void onInit(final int status) {
+		tts.setLanguage(Locale.ENGLISH);
+		if(speed >= 0)
+			tts.setSpeechRate(speed / 10);
+	}
+
+	@Override
+	public boolean onTouch(final View v, final MotionEvent event) {
+		if(event.getAction() == MotionEvent.ACTION_DOWN) {
+			if(pos == string.length() - 1) {
+				finish();
+				return false;
+			}
+			handler.post(Updater);
+			handler.post(Updater);
+		}
+		return false;
+	}
+}
---
+++ src/net/teknoraver/ics/Main.java
@@ -0,0 +1,105 @@
+package net.teknoraver.ics;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.SeekBar;
+
+public class Main extends Activity implements OnClickListener {
+	private SharedPreferences pref;
+	private EditText et;
+	private CheckBox sf, st, sp, touch;
+	private SeekBar speed;
+	private static final String showf = "showf", showt = "showt", speak = "speak", tta = "tta", speedv = "speedv", txt = "txt";
+
+	@Override
+	public void onCreate(final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.input);
+
+		pref = PreferenceManager.getDefaultSharedPreferences(this);
+
+		et = (EditText)findViewById(R.id.text);
+		sf = (CheckBox)findViewById(R.id.showflag);
+		st = (CheckBox)findViewById(R.id.showtext);
+		sp = (CheckBox)findViewById(R.id.speak);
+		touch = (CheckBox)findViewById(R.id.touch);
+		speed = (SeekBar)findViewById(R.id.speed);
+
+		touch.setOnClickListener(this);
+
+		sf.setChecked(pref.getBoolean(showf, true));
+		st.setChecked(pref.getBoolean(showt, true));
+		sp.setChecked(pref.getBoolean(speak, true));
+		final boolean ttabool = pref.getBoolean(tta, false);
+		touch.setChecked(ttabool);
+		speed.setProgress(pref.getInt(speedv, 5));
+		speed.setEnabled(!ttabool);
+		et.setText(pref.getString(txt, ""));
+
+		findViewById(R.id.show).setOnClickListener(this);
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		final SharedPreferences.Editor editor = pref.edit();
+
+		editor.putBoolean(showf, sf.isChecked());
+		editor.putBoolean(showt, st.isChecked());
+		editor.putBoolean(speak, sp.isChecked());
+		editor.putBoolean(tta, touch.isChecked());
+		editor.putInt(speedv, speed.getProgress());
+		editor.putString(txt, et.getText().toString());
+
+		editor.commit();
+	}
+
+	@Override
+	public void onClick(final View view) {
+		if(view.getId() == R.id.show) {
+			if(!sf.isChecked() && !st.isChecked() && !sp.isChecked())
+				return;
+
+			int showspeed = -1;
+			if(!touch.isChecked())
+				showspeed = 15 - speed.getProgress();
+
+			startActivity(new Intent(this, ICS.class)
+				.putExtra(ICS.stringcmd, (" " + et.getText()).toUpperCase() + " ")
+				.putExtra(ICS.flagcmd, sf.isChecked())
+				.putExtra(ICS.textcmd, st.isChecked())
+				.putExtra(ICS.speakcmd, sp.isChecked())
+				.putExtra(ICS.speedcmd, showspeed));
+		} if(view.getId() == R.id.touch)
+			speed.setEnabled(!touch.isChecked());
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+//		if(item.getItemId() == R.id.about) {
+			new AlertDialog.Builder(this)
+				.setTitle(R.string.about)
+				.setMessage(R.string.aboutt)
+				.setIcon(android.R.drawable.ic_dialog_info)
+				.show();
+			return true;
+//		}
+//		return false;
+	}
+}
