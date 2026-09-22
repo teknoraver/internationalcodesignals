2d13b79c7623ac857120ce5f9847f105fbf8c32a
Matteo Croce <technoboy85@gmail.com>
2026-09-22 15:16:34 +0200

    SDK update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,24 +1,46 @@
+import java.util.Properties
+
 plugins {
     id("com.android.application")
 }
 
+val keystorePropertiesFile = rootProject.file("keystore.properties")
+val keystoreProperties = Properties()
+val hasKeystore = keystorePropertiesFile.exists()
+if (hasKeystore) {
+    keystoreProperties.load(keystorePropertiesFile.inputStream())
+}
+
 android {
     namespace = "net.teknoraver.ics"
-    compileSdk = 34
-    buildToolsVersion = "34.0.0"
+    compileSdk = 37
+    buildToolsVersion = "37.0.0"
 
     defaultConfig {
         applicationId = "net.teknoraver.ics"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 37
         versionCode = 21
         versionName = "1.7"
+    }
+    signingConfigs {
+        if (hasKeystore) {
+            create("release") {
+                storeFile = file(keystoreProperties["storeFile"] as String)
+                storePassword = keystoreProperties["storePassword"] as String
+                keyAlias = keystoreProperties["keyAlias"] as String
+                keyPassword = keystoreProperties["keyPassword"] as String
+            }
+        }
     }
     buildTypes {
         release {
             isMinifyEnabled = true
             isShrinkResources = true
-            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            if (hasKeystore) {
+                signingConfig = signingConfigs.getByName("release")
+            }
         }
     }
 }
